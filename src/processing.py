e1dd29699002c47a6dbd9f112e0d06b20df7ddc6
Svetlana Kotova <skazka1977@mail.ru>
2024-09-15 14:22:25 +1000

    sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any
 
 
@@ -19,3 +20,23 @@ list_of_dictionaries = [
 ]
 filtered_list = filter_by_state(list_of_dictionaries)
 print(filtered_list)
+
+
+def sort_by_date(list_of_dictionaries: list[dict[str, Any]], revers: bool = True) -> list[dict[str, Any]]:
+    """Сортируем список словарей по дате (по возрастанию или убыванию)"""
+    sorted_list = sorted(
+        list_of_dictionaries, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=revers
+    )
+    return sorted_list
+
+
+list_of_dictionaries = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+]
+
+
+sorted_list = sort_by_date(list_of_dictionaries)
+print(sorted_list)
